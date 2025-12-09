6b029fa0b45a89d23b274e7b892a45f49a884007
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2025-12-09 09:38:48 +0000

    added nob test

--- nob.c
+++ nob.c
@@ -4,8 +4,26 @@
 #define BUILD_FOLDER "build/"
 #define SRC_FOLDER "src/"
 
+#define STRING_INTERNER_IMPL
+#include "src/string_interner.h"
+
+
+uint64_t run_tests() {
+  uint64_t errors = 0;
+  errors += string_interner_tests();
+  return errors;
+}
+
+
 int main(int argc, char **argv) {
   NOB_GO_REBUILD_URSELF(argc, argv);
+
+  if (argc == 2 && !strcmp(argv[1], "test")) {
+    uint64_t errors = run_tests();
+    printf("\nIdentified %lu failures\n", errors);
+    return errors;
+  }
+  
   if (!nob_mkdir_if_not_exists(BUILD_FOLDER))
     return 1;
   Nob_Cmd cmd = {0};
